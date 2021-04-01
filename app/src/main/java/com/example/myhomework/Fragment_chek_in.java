09b70d370676ec144e290fd30239b6168fbe41f1
Ильяс Залялов <zalyalovir@gmail.com>
2021-04-01 14:10:20 +0300

    Взаймодействие с фрагментами

---
+++ app/src/main/java/com/example/myhomework/Fragment_chek_in.java
@@ -0,0 +1,21 @@
+package com.example.myhomework;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment_chek_in extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chek_in, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/myhomework/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.myhomework;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.analyzer.VerticalWidgetRun;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static android.view.View.GONE;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//    private Fragment_enter fragment_enter;
+//    private Fragment_chek_in fragment_chek_in;
+//
+//    private FragmentManager manager;
+//    private FragmentTransaction transaction;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        manager = getSupportFragmentManager();
+//
+//        fragment_enter = new Fragment_enter();
+//        fragment_chek_in = new Fragment_chek_in();
+    }
+
+//    public void onClickFragment1 (View v){
+//        transaction = manager.beginTransaction();
+//
+//        switch (v.getId()) {
+//            case R.id.button5 :
+//                transaction.add(R.id.conteiner,fragment_enter);
+//                break;
+//        }
+//
+//        transaction.commit();
+//
+//    }
+//
+//    public void onClickFragment2 (View v){
+//        transaction = manager.beginTransaction();
+//
+//        switch (v.getId()) {
+//            case R.id.button6 :
+//                transaction.add(R.id.conteiner,fragment_chek_in);
+//                break;
+//        }
+//
+//        transaction.commit();
+//
+//    }
+//
+  public void Change (View v) {
+        Fragment fragment = null;
+
+        switch (v.getId()){
+            case R.id.button5:
+                fragment = new Fragment_enter();
+                break;
+            case R.id.button6:
+                fragment = new Fragment_chek_in();
+                break;
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.conteiner,fragment);
+        ft.commit();
+  }
+}
